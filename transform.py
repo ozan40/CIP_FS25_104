5f5c970eab9830406fbee6654a8dede077daa9c5
ozan40 <58996323+ozan40@users.noreply.github.com>
2025-03-21 10:18:31 +0100

    Resolve merge conflict in transform.py

--- transform.py
+++ transform.py
@@ -4,11 +4,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     # load data
-<<<<<<< HEAD
-    car_df = pd.read_csv("crawled_output.csv", sep=";")
-=======
     car_df = pd.read_csv("Data/crawled_output.csv", sep=";")
->>>>>>> Ozan
 
     # clean Data
     cleaner = transform.DataCleaner(car_df)
@@ -27,9 +23,5 @@ if __name__ == "__main__":
     print(outlier_information)
     print(enriched_df.dtypes)
     print(enriched_df['YearMonth'].head())
-<<<<<<< HEAD
-    #enriched_df.to_csv("transformed_output.csv", sep=";")
-=======
     #enriched_df.to_csv("Data/transformed_output.csv", sep=";")
->>>>>>> Ozan
 
