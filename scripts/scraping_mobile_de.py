1095199a86092215b16a99d693f6c771a65a717b
Oliver Zingg <oliver9402@gmail.com>
2025-03-22 02:27:53 +0100

    links

--- scripts/scraping_mobile_de.py
+++ scripts/scraping_mobile_de.py
@@ -34,12 +34,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from crawler.CrawledCar import CrawledCar
+from urllib.parse import urljoin
+#from crawler.CrawledCar import CrawledCar
 
 url = "https://suchen.mobile.de/fahrzeuge/search.html?dam=false&isSearchRequest=true&ref=quickSearch&s=Car&sb=rel&vc=Car"
 
 print(url)
 
+class CrawledCar():
+    # Konstruktur initialisieren und brand, model, description, price, kilometer, gear, year, fuel, hp, etc. übergeben
+    def __init__(self, brand = None, model= None, car_info= None, user_text= None, price= None, price_evaluation= None,
+                 km= None, gear= None, date= None, fuel= None, power= None,
+                 consumption= None, co2= None):
+        self.brand = brand
+        self.model = model
+        self.car_info = car_info
+        self.user_text = user_text
+        self.price = price
+        self.price_evaluation = price_evaluation
+        self.kilometer = km
+        self.gear = gear
+        self.date = date
+        self.fuel = fuel
+        self.power = power
+        self.consumption = consumption
+        self.co2 = co2
 
 class carFetcher():
 
@@ -61,19 +80,29 @@ class carFetcher():
         all_cars_html = soup.find_all('article', class_="A3G6X vTKPY")
         #fuel_keywords = ["Benzin", "Diesel", "Elektro", "Autogas (LPG)", "Elektro/Benzin", "Elektro/Diesel", "Sonstige",
         #                "Erdgas (CNG)", "Ethanol", "Wasserstoff"]
+
         cars = []
-        for car in all_cars_html:  # change this not hard coded
-            #idx = 1
+
+        url = 'https://suchen.mobile.de/'
+        all_links = soup.find_all('a', class_='FWtU1 YIC4W rqEvz', href = True)
+        
+        for link in all_links: 
+            full_url = urljoin(url, link['href'])
+            print(full_url)
+
+        for car in all_cars_html[2]:  # change this not hard coded
 
             list_info = carFetcher.extract_info(car, 'div', 'HaBLt')
             price = carFetcher.extract_info(car, 'span', 't5RmH')
             brand = carFetcher.extract_info(car, 'span', 'LBG5d')
+           
             if list_info:
                 list_info = list_info.split("•")
                 list_info = [element.replace("\xa0", " ").strip() for element in list_info]
                 hp = next((element for element in list_info if " kW " in element), None)
                 km = next((element for element in list_info if " km" in element), None)
                 year = next((element for element in list_info if "EZ " in element), None)
+           
             else:
                 hp = None
                 km = None
@@ -81,12 +110,15 @@ class carFetcher():
 
                 # print(f"Extracted Info: {hp}, {km}, {year}, {price}, {brand}")  # Debugging
 
-            crawled_object = CrawledCar(power = hp, kilometer = km, date = year, price = price, brand = brand)
+            crawled_object = CrawledCar(power = hp, km = km, date = year, price = price, brand = brand)
             cars.append(crawled_object)
             #idx += 1
 
         #print(f'Returning {len(cars)} cars from this page")
         return cars
+    
+
+        
 
     def fetch_multiple_pages(self, url, max_pages=1):
         self.driver.get(url)
@@ -119,11 +151,9 @@ def main():
     car_list = test.fetch_multiple_pages(url)
 
     print(car_list)
-    #for car in car_list:
-    #    print(vars(car))
-
+    for car in car_list:
+        print(vars(car))
     return car_list
-
 
 if __name__ == '__main__':
     dataset = main()
