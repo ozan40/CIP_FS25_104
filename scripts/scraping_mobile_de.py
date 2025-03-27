8d6a7dbbe52fb48a8502271f18ab42ee654c55aa
Oliver Zingg <oliver9402@gmail.com>
2025-03-27 22:30:48 +0100

    new

--- scripts/scraping_mobile_de.py
+++ scripts/scraping_mobile_de.py
@@ -26,7 +26,10 @@
 # -----------------------------
 
 import re
+import os
+import sys
 import time
+import json
 import csv
 import requests
 from bs4 import BeautifulSoup
@@ -37,13 +40,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from fake_useragent import UserAgent
+
 import random
 from urllib.parse import urljoin
 #from crawler.CrawledCar import CrawledCar
 
-url = "https://suchen.mobile.de/fahrzeuge/search.html?dam=false&isSearchRequest=true&ref=quickSearch&s=Car&sb=rel&vc=Car"
 
-print(url)
 
 class CrawledCar():
     # Konstruktur initialisieren und brand, model, description, price, kilometer, gear, year, fuel, hp, etc. übergeben
@@ -68,13 +71,20 @@ class carFetcher():
 
     def __init__(self):
         # Set up Firefox options
-        firefox_options = Options()
-        firefox_options.set_preference("permissions.default.image", 2)  # Disable images
-        firefox_options.set_preference("dom.ipc.plugins.enabled.libflashplayer.so", "false")  # Disable Flash
-        firefox_options.headless = True  # Enable headless mode for faster execution
+        options = Options()
+        
+        options.set_preference("permissions.default.image", 2)  # Disable images
+        options.set_preference("dom.ipc.plugins.enabled.libflashplayer.so", "false")  # Disable Flash
+        options.set_preference("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+        options.set_preference("dom.webdriver.enabled", False)  # Disable automation flag
+        options.set_preference("useAutomationExtension", False)
+        options.headless = True  # Enable headless mode for faster execution
+        ua = UserAgent()
+        user_agent = ua.random
+        options.add_argument(f'user-agent={user_agent}')
 
         service = Service('/usr/local/bin/geckodriver')
-        self.driver = webdriver.Firefox(service=service, options=firefox_options)
+        self.driver = webdriver.Firefox(service=service, options=options)
 
         #self.driver = webdriver.Firefox()
         self.all_cars = []
@@ -85,48 +95,91 @@ class carFetcher():
             return car_html.find(tag, class_=class_input).text
         except AttributeError:
             return None
+    
+    def extract_brand_names(self):
+        # Load the JSON file
+        with open("car_brand.json", "r") as file:
+            data = json.load(file)
 
-    def following_link(self, soup):
+        # Extract the "ms" list from the nested "data" key
+        ms_list = data.get("data", {}).get("ms", [])
+        # Extract only the 'n' (name) values
+        brand_names = [item.get("n") for item in ms_list]
+        return brand_names
+    
+    def extract_names(self, brand_list, brand):
+        
+        if brand:
+            brand_name = []
+            type_name = []
 
+            for element in brand.split(" "):
+                if element in brand_list:
+                    brand_name.append(element)
+                else:
+                    type_name.append(element)
+            return brand_name, type_name
+        else:
+            return None, None
+        
+    def following_link(self, car_html):
+        """
+        Follows individual links of each car listing to detailed site. This access is blocked possibily due to bot detection. 
+        No hidden api can be found to sidestep this approach :(
+        This function is not further used.
+        """
  
 
         url = 'https://suchen.mobile.de/'
-        link = soup.find('a', class_='FWtU1 YIC4W rqEvz', href = True)
+        link = car_html.find('a', class_='FWtU1 YIC4W rqEvz', href = True)
         
         try: 
             full_url = urljoin(url, link['href'])
             print("fetiching:" , {full_url} )
-            try: 
-                req = requests.get(full_url, timeout=10)  # Add a timeout to prevent hanging
-                req.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-                print(req[1])
-            except requests.exceptions.RequestException as e:
-                print(f"Failed to fetch {full_url}: {e}")
+
         except: 
             return None
             
         try:
-                # Navigate to link 
-                self.driver.get(full_url)
-                soup_sublink = BeautifulSoup(self.driver.page_source, 'html.parser')
-            
-                try:
+               # Wait for the page to load and fetch cars
+            response = requests.get(full_url)
+            print(response.status_code)
 
-                    next_button = WebDriverWait(self.driver, 10).until(
-                     EC.visibility_of_element_located((By.CSS_SELECTOR, "div.VXw4m button.FWtU1"))
-)
-                    #next_button = WebDriverWait(self.driver, 10).until(
-                    #EC.element_to_be_clickable((By.CSS_SELECTOR, "div.VXw4m button.FWtU1"))
-                    #)
-                    next_button.click()
-                    print("found button")
-                except:
-                    print("No 'Mehr Anzeigen' button found, stopping.")
+            self.driver.get(full_url)
+            time.sleep(random.uniform(8, 15))
+
+            
+            try:
+                # Wait for the "Mehr Anzeigen" button to be present
+
+                mehr_anzeigen_button = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, "//button[@type='button' and contains(@class, 'FWtU1') and contains(@class, 'drxl5') and contains(@class, 'XY6XP') and @role='link']"))
+                )
+                # Get the new URL
+                new_url = self.driver.current_url
+                print("New URL:", new_url)
+                # Click the button using JavaScript
+                self.driver.execute_script("arguments[0].click();", mehr_anzeigen_button)
+                new_url_afer_click = self.driver.current_url
+                print("New URL:", new_url_afer_click)
+                # Wait for the page to load after clicking
+                WebDriverWait(self.driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//button[@type='button' and contains(@class, 'FWtU1') and contains(@class, 'drxl5') and contains(@class, 'XY6XP') and @role='link']"))
+                )
+                print(f"inspect button {self.driver.current_url}")
                 
-                fuel_type = carFetcher.extract_info(soup_sublink, 'dd', 'nuAmT')
-                print(fuel_type)
+            except Exception as e:
+                print(f"Error interacting with 'Mehr Anzeigen' button: {e}")
+                #with open("page_source.html", "w", encoding="utf-8") as file:
+                #     file.write(self.driver.page_source)
+                #print("HTML saved to page_source.html")
+                
+                
         except:
             print("ups didn't work")   
+        fuel_type = carFetcher.extract_info(self.driver, 'dd', 'nuAmT')
+        print(f"Fuel type: {fuel_type}")
+
         return fuel_type
 
     def fetch_page(self):
@@ -137,15 +190,28 @@ class carFetcher():
 
         cars = []
         
-        fuel_keywords = ["Benzin", "Diesel", "Elektro", "Autogas (LPG)", "Elektro/Benzin", "Elektro/Diesel", "Sonstige",
-                         "Erdgas (CNG)", "Ethanol", "Wasserstoff"]
+        fuel_keywords = ["Benzin", "Diesel", "Elektro", "Autogas (LPG)", "Hybrid (Diesel/Elektro)","Hybrid (Benzin/Elektro)", "Elektro/Diesel", "Andere",
+                         "Erdgas (CNG)", "Ethanol (FFV, E85 etc.)", "Wasserstoff", "Plug-in-Hybrid"]
 
         for car in all_cars_html: 
 
             list_info = carFetcher.extract_info(car, 'div', 'HaBLt')
             price_info = carFetcher.extract_info(car, 'span', 't5RmH')
-            price = price_info.replace("\xa0", " ").strip() if price_info else None
+            price = re.sub(r'\D', '', price_info) if price_info else None
+            print(f"{price=}")
             brand = carFetcher.extract_info(car, 'span', 'LBG5d')
+            
+            if brand:
+                brand_name = brand.split(" ")[0]
+
+                type_name  = " ".join(brand.split(" ")[1:])
+
+            else:
+                brand_name = None
+                type_name = None
+            #split brand and type 
+            #brand_list = self.extract_brand_names()
+            #brand_name, type_name=  self.extract_names(self, brand_list, brand)
             car_info = carFetcher.extract_info(car, 'span', 'Z_aNr')
             price_evaluation = carFetcher.extract_info(car, 'div', '_u77E XtrJR')
             #fuel = self.following_link(car)
@@ -153,9 +219,18 @@ class carFetcher():
             if list_info:
                 list_info = list_info.split("•")
                 list_info = [element.replace("\xa0", " ").strip() for element in list_info]
-                hp = next((element for element in list_info if " kW " in element), None)
+                hp = next((element for element in list_info if " PS" in element), None)
+                if hp:
+                    hp = re.search(r'\((\d+)\sPS\)', hp).group(1)
+                    print(f"{hp=}")
                 km = next((element for element in list_info if " km" in element), None)
+                if km:
+                    km = re.sub(r'\D', '', km)
+                    print(f"{km=}")
                 year = next((element for element in list_info if "EZ " in element), None)
+                if year: 
+                    year = re.sub(r'[a-zA-Z]', '', year).strip()
+                    print(f"{year=}")
                 fuel = next((element for element in list_info if element in fuel_keywords), None)
 
            
@@ -164,9 +239,10 @@ class carFetcher():
                 km = None
                 year = None
                 fuel = None
+                
 
                 # print(f"Extracted Info: {hp}, {km}, {year}, {price}, {brand}")  # Debugging
-            crawled_object = CrawledCar(power = hp, km = km, date = year, price = price, brand = brand, fuel = fuel, car_info= car_info,
+            crawled_object = CrawledCar(power = hp, km = km, date = year, price = price, brand = brand_name, model = type_name, fuel = fuel, car_info= car_info,
                                         price_evaluation=price_evaluation)
             cars.append(crawled_object)
             #idx += 1
@@ -174,28 +250,11 @@ class carFetcher():
         #print(f'Returning {len(cars)} cars from this page")
         return cars
 
-        
-        
-    
-    
-    def fetch_multiple_pages(self, url, max_pages=50):
-        self.driver.get(url)
-        #print(f"Fetching from: {url}")
-        for i in range(max_pages):
-            # Wait for the page to load and fetch cars
-            time.sleep(5)
-            cars_on_page = WebDriverWait(self.driver, 10).until(
-                lambda driver: self.fetch_page()  # Fetch cars when the page is ready: explain this code later
-        )
-            if cars_on_page:
-                self.all_cars.extend(cars_on_page)
-            else:
-                break
+    def interact_next_button(self):
+        """Interacts with the 'Next' button up to max_interactions times."""
 
-            
-            try:
+        try:
                 # Wait for the "Next" button to be present
-
                 next_button = WebDriverWait(self.driver, 5).until(
                     EC.presence_of_element_located((By.XPATH, "//button[@data-testid='pagination:next']"))
                 )
@@ -203,17 +262,49 @@ class carFetcher():
                 # Click the button using JavaScript
                 self.driver.execute_script("arguments[0].click();", next_button)
 
-                # Wait for the page to load after clicking
-                WebDriverWait(self.driver, 5).until(
-                    EC.presence_of_element_located((By.XPATH, "//button[@data-testid='pagination:next']"))
+                # Wait for the page to load
+                time.sleep(5)
+        except Exception as e:
+                print(f"No 'Next' button found or error occurred: {e}")
+                      
+        
+    def change_category_url(self, url, idx):
+        """Starting URL should by one category to ensure corerect link.
+        Adjusts the URL to increment the page number by 1.
+        """
+
+        categories = ["OffRoad", "EstateCar", "Van", "SportsCar", "SmallCar",
+                  "Limousine", "OtherCar"]
+        # Replace the old page number with the new one
+        changed_url =re.sub(r'c=[^&]+', f'c={categories[idx]}', url)
+    
+        return changed_url
+   
+    
+    def fetch_multiple_pages(self, url, max_pages=50):
+        for cat in range(7):
+            changed_url = self.change_category_url(url, cat)
+            self.driver.get(changed_url)
+            #print(f"Fetching from: {url}")
+            for i in range(max_pages):
+                # Wait for the page to load and fetch cars
+                time.sleep(5)
+                cars_on_page = WebDriverWait(self.driver, 10).until(
+                    lambda driver: self.fetch_page()  # Fetch cars when the page is ready: explain this code later
                 )
-                print(f"inspect button {self.driver.current_url}")
-            except Exception as e:
-                print(f"Error interacting with 'Next' button: {e}")
-                #with open("page_source.html", "w", encoding="utf-8") as file:
-                #     file.write(self.driver.page_source)
-                #print("HTML saved to page_source.html")
-                break
+                if cars_on_page:
+                    self.all_cars.extend(cars_on_page)
+                else:
+                    break
+
+            
+                try:
+                    # Use the 'Next' button to navigate
+                    self.interact_next_button()
+                except:
+                    print("Couldn't interact with next button")
+             
+
             
 
         self.driver.quit()
@@ -222,21 +313,33 @@ class carFetcher():
 
 
 def main():
-    test = carFetcher()
-    car_list = test.fetch_multiple_pages(url)
+    # Set the working directory to the directory of the current file
+    #script_dir = os.path.dirname(os.path.abspath(__file__))
+    #os.chdir(script_dir)
+    print(f"Working directory set to: {os.getcwd()}")
+#    Print the sys.path to see what paths are appended
+    print("Python sys.path:")
+    for path in sys.path:
+        print(f"  {path}")
+    print("-"*20)   
+    url = "https://suchen.mobile.de/fahrzeuge/search.html?c=Cabrio&dam=false&isSearchRequest=true&ref=dsp&s=Car&sb=rel&vc=Car"
+    mobile_de = carFetcher()
+    car_list = mobile_de.fetch_multiple_pages(url)
 
+    # saves it to root becaus i added sys.path.append("path to root") to this script trying to import crawler
     # Export car_list to a CSV file
-    with open('car_list.csv', mode='w', newline='', encoding='utf-8') as file:
+    with open('car_mobile.csv', mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
 
         # Write the header row (attribute names)
         if car_list:
             writer.writerow(vars(car_list[0]).keys())
 
-        # Write the data rows (attribute values)
         for car in car_list:
-            writer.writerow(vars(car).values())
+             # Check if the row is not empty (i.e., at least one value is not None or empty)
+            row = list(vars(car).values())
+            if any(row):  # If any value in the row is non-empty
+                writer.writerow(row)
 
 if __name__ == '__main__':
     dataset = main()
-
