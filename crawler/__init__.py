21206ccbc0503d648bfc4d4f98ead5c0e53a383e
ozan40 <58996323+ozan40@users.noreply.github.com>
2025-04-01 19:22:42 +0200

    modify Thomas files within crawler package

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,8 +1,9 @@
 # mit diesem __all__ sage ich, in diesem Package habe ich diese Files und damit erlaube ich,
 # dass ich diese importieren kann.
-__all__ = ["CrawledCar","CarsFetcher"]
+__all__ = ["CrawledCar","CarsFetcher","WebAutomation", "CarFetcher"]
 
 # Damit das alles klappt, muss ich hier im __init__ file noch sage, hey importiere doch dieses Modul.
 # Python betrachtet also nicht diese __all__ variable, sondern wir müssen noch zusätzlichi import Befehle platzieren,
 from .CarsFetcher import CarsFetcher
 from .CrawledCar import CrawledCar
+from .Auto_de_scraper import WebAutomation, CarFetcher
